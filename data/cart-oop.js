fae2fce8cb43965f53ed91d08f19c4de08a3c9b4
John Cajigal WebDev <johncajigal.webdev@gmail.com>
2025-06-03 18:38:07 +0800

    .

---
+++ data/cart-oop.js
@@ -0,0 +1,66 @@
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  //if the cart is empty, this is the default value
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "3",
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  //s-s-making sure quantity adds up. No duplication of properties in Cart.js
+  let matchingItem;
+  cart.forEach((cartitem) => {
+    if (productId === cartitem.productId) {
+      matchingItem = cartitem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: "1",
+    });
+  }
+  //e-e-making sure quantity adds up. No duplication of properties in Cart.js
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartitem) => {
+    if (productId === cartitem.productId) {
+      matchingItem = cartitem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
