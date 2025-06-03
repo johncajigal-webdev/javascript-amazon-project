0719d74fcb29ae977ca5a2b21aae862c7a324f8b
John Cajigal WebDev <johncajigal.webdev@gmail.com>
2025-06-04 00:23:19 +0800

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,88 @@
+class Cart {
+  //shortcut// cartItems = undefined;
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+
+  //shorthand method(next line): loadFromStorage(){
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      //if the cart is empty, this is the default value
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "3",
+        },
+      ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    //s-s-making sure quantity adds up. No duplication of properties in Cart.js
+    let matchingItem;
+    this.cartItems.forEach((cartitem) => {
+      if (productId === cartitem.productId) {
+        matchingItem = cartitem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: "1",
+      });
+    }
+    //e-e-making sure quantity adds up. No duplication of properties in Cart.js
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+    this.cartItems.forEach((cartitem) => {
+      if (productId === cartitem.productId) {
+        matchingItem = cartitem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart('cart-oop'); // This generates a new object using our class;
+const businessCart = new Cart('cart-business');
+
+
+
+console.log(cart);
+console.log(businessCart);
