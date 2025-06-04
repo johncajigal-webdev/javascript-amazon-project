0a1288157b3dd4f3de37240682ae63f8c6e2ae1c
John Cajigal WebDev <johncajigal.webdev@gmail.com>
2025-06-05 01:40:56 +0800

    convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,14 +3,52 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-
-loadProducts(()=>{
-  
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value 1"); //value 1
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve(); //no value = undefined
+    });
+  }),
+]).then((values)=>{
+  console.log(values); //Output: Array [ "value 1", undefined ]
   renderOrderSummary();
   renderPaymentSummary();
-
-  
 });
 
-  
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value 1");
+  });
+})
+  .then((value) => {
+    console.log(value); //Output: value 1
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+
+/*
+loadProducts(() => {
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+});
+*/
